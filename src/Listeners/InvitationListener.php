746accf152775e92937c22751dfbae8340899f67
Jon Nicholson <macro@drjonnicholson.com>
2016-12-13 14:51:25 +0000

    Added methods as abstract

--- src/Listeners/InvitationListener.php
+++ src/Listeners/InvitationListener.php
@@ -8,4 +8,16 @@ abstract class InvitationListener
     {
         return call_user_func($event, $invitation);
     }
+
+    public abstract function pending($invitation);
+
+    public abstract function issued($invitation);
+
+    public abstract function successful($invitation);
+
+    public abstract function revoked($invitation);
+
+    public abstract function rejected($invitation);
+
+    public abstract function expired($invitation);
 }
