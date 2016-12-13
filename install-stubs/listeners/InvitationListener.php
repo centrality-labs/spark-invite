e5c8ec2b31b657229b53c733ad7d6a62f24e198d
Jon Nicholson <macro@drjonnicholson.com>
2016-12-13 16:20:31 +0000

    Reformat

--- install-stubs/listeners/InvitationListener.php
+++ install-stubs/listeners/InvitationListener.php
@@ -7,29 +7,38 @@ use ZiNETHQ\SparkInvite\Listeners\InvitationListener as Listener;
 class InvitationListener extends Listener
 {
     public function invite($invitation)
-    { }
+    {
+    }
 
     public function reinvite($invitation)
-    { }
+    {
+    }
 
     public function pending($invitation)
-    { }
+    {
+    }
 
     public function issued($invitation)
-    { }
+    {
+    }
 
     public function accepted($invitation)
-    { }
+    {
+    }
 
     public function successful($invitation)
-    { }
+    {
+    }
 
     public function revoked($invitation)
-    { }
+    {
+    }
 
     public function rejected($invitation)
-    { }
+    {
+    }
 
     public function expired($invitation)
-    { }
+    {
+    }
 }
